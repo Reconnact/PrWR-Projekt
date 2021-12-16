be8a0c42d6a04136e9fa3eb9bcd3f2665d361d89
Hermann Witte <witteh@bzz.ch>
2021-12-16 13:42:21 +0100

    pics

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//Get the button:
 mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
@@ -14,7 +12,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0; 
 }
